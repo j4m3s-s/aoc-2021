bab205fc18774a09754882d6f35590f1e0720c69
James <me@j4m3s.eu>
2021-12-02 20:57:36 +0100

    feat(2): add solutions
    Signed-off-by: James <me@j4m3s.eu>

---
+++ 2/solution-1.cc
@@ -0,0 +1,68 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+
+#include <vector>
+using std::vector;
+
+#include <string>
+using std::string;
+
+enum class ActionType {
+	Forward,
+	Down,
+	Up,
+};
+
+struct Action {
+	ActionType type;
+	int value;
+};
+
+int solve(vector<Action>& input) {
+	int depth = 0;
+	int horizontal_position = 0;
+
+	for (auto v : input) {
+		if (v.type == ActionType::Forward) {
+			horizontal_position += v.value;
+		} else if (v.type == ActionType::Up) {
+			depth -= v.value;
+		} else if (v.type == ActionType::Down) {
+			depth += v.value;
+		} else {
+			cout << "ABORT\n";
+		}
+	}
+
+	return depth * horizontal_position;
+}
+
+int main(void) {
+	vector<Action> input;
+
+	while (1) {
+		if (!cin)
+			break;
+
+		Action action;
+
+		string tmp;
+		cin >> tmp;
+		if (tmp == "forward")
+			action.type = ActionType::Forward;
+		else if (tmp == "down")
+			action.type = ActionType::Down;
+		else if (tmp == "up")
+			action.type = ActionType::Up;
+		else
+			break; // last line is '\n'
+
+		cin >> action.value;
+
+		input.push_back(action);
+		cout << tmp << ' ' << action.value << '\n';
+	}
+
+	cout << solve(input) << std::endl;
+}
